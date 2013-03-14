cfc965acbfbd1a07e73efca73971251261bc16f9
Charles Hsu <hulu2020@gmail.com>
2013-03-14 11:15:43 +0800

    add CrystalSANServer

--- CrystalSANServer/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $host= gethostname();
-    $ip = gethostbyname($host);
-    //$ip = gethostbyname(trim(`hostname`));
-    echo "hostname = $host";
-    echo "CrystalSAN Server installed on $ip successfully";
-?>
--- CrystalSANServer/sqlite3_exec.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db = new SQLite3('mysqlitedb.db');
-
-$db->exec('CREATE TABLE bar (bar STRING)');
-?>
