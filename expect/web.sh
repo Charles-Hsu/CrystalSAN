882f97e2d04e2b7248c53ecb0a66087a7c688515
Charles Hsu <hulu2020@gmail.com>
2013-05-03 12:14:14 +0800

    add

---
+++ expect/web.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  Script.sh
+#  CrystalSAN
+#
+#  Created by Charles Hsu on 3/18/13.
+#  Copyright (c) 2013 Charles Hsu. All rights reserved.
+
+cd /Library/WebServer/Documents/CrystalSANServer/
